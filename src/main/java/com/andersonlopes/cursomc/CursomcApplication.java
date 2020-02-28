f046feea5e3c199e29d6320422c3c112ad7b7e40
Anderson Lopes <andersoncleytonalveslopes@gmail.com>
2020-02-28 10:50:43 -0300

    Criado CategoriaService e CategoriaRepository e alterado o Resource

--- src/main/java/com/andersonlopes/cursomc/CursomcApplication.java
+++ src/main/java/com/andersonlopes/cursomc/CursomcApplication.java
@@ -1,13 +1,25 @@
 package com.andersonlopes.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.andersonlopes.cursomc.domain.Categoria;
 
 @SpringBootApplication
 public class CursomcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.addAll(Arrays.asList(cat1, cat2));
 	}
 
 }
--- src/main/java/com/andersonlopes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andersonlopes/cursomc/resources/CategoriaResource.java
@@ -4,24 +4,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andersonlopes.cursomc.domain.Categoria;
+import com.andersonlopes.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1,"Informática");
-		Categoria cat2 = new Categoria(2,"Escritório");
-
-		List<Categoria> categorias = new ArrayList<>();
-		categorias.addAll(Arrays.asList(cat1, cat2));
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		return categorias;
+		Categoria obj = service.buscar(id);
+		
+		return ResponseEntity.ok().body(obj);
 	}
 }
