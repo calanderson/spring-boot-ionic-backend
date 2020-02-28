b3ff739dbcf157e58dad1117b25784692948a020
Anderson Lopes <andersoncleytonalveslopes@gmail.com>
2020-02-28 10:01:06 -0300

    Testando a primeira classe de domínio Categoria

--- src/main/java/com/andersonlopes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andersonlopes/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.andersonlopes.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.andersonlopes.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funfando!!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.addAll(Arrays.asList(cat1, cat2));
+		
+		return categorias;
 	}
 }
