54a11c77f56c18896da3321d8bf1d91101418c52
Anderson Lopes <andersoncleytonalveslopes@gmail.com>
2020-03-03 14:04:52 -0300

    Criação das tabelas Estado e Cidade

--- src/main/java/com/andersonlopes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andersonlopes/cursomc/resources/CategoriaResource.java
@@ -1,9 +1,5 @@
 package com.andersonlopes.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +21,6 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
 		Categoria obj = service.buscar(id);
-		
 		return ResponseEntity.ok().body(obj);
 	}
 }
