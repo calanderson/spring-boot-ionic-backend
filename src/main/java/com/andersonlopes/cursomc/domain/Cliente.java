3d90b41e1e8a8f01c0e7e85be4597d4d0961d5f3
Anderson Lopes <andersoncleytonalveslopes@gmail.com>
2020-03-04 13:52:37 -0300

    Atualização dos Annotations JsonIgnore

--- src/main/java/com/andersonlopes/cursomc/domain/Cliente.java
+++ src/main/java/com/andersonlopes/cursomc/domain/Cliente.java
@@ -15,7 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.andersonlopes.cursomc.domain.enums.TipoCliente;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Cliente implements Serializable{
@@ -36,7 +36,7 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
