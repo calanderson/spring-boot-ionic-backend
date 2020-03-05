a84599e7011719a5df23e449416784f2a4c966ee
Anderson Lopes <andersoncleytonalveslopes@gmail.com>
2020-03-05 17:14:57 -0300

    Subtotal e total do Pedido

--- src/main/java/com/andersonlopes/cursomc/domain/ItemPedido.java
+++ src/main/java/com/andersonlopes/cursomc/domain/ItemPedido.java
@@ -31,6 +31,10 @@ public class ItemPedido implements Serializable{
 		this.id.setProduto(produto);
 	}
 	
+	public Double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+	
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
