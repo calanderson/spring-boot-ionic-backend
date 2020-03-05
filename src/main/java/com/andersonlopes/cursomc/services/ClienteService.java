f27bed1e32b1c3dcf8f9d47f65b151a19af6e083
Anderson Lopes <andersoncleytonalveslopes@gmail.com>
2020-03-05 13:58:21 -0300

    Ajustando e testando a deleção de cliente

--- src/main/java/com/andersonlopes/cursomc/services/ClienteService.java
+++ src/main/java/com/andersonlopes/cursomc/services/ClienteService.java
@@ -58,7 +58,7 @@ public class ClienteService {
 		try {
 			clienteRepository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir um cliente que há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir um cliente que há pedidos relacionadas");
 		}
 	}
 
